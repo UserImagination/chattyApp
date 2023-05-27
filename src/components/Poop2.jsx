43204749e3b0d32f4128ae3f812d5cca48f57dc0
Matthew Craig <mgcraig78@gmail.com>
2023-05-26 20:32:57 -0700

    added a new poop component

---
+++ src/components/Poop2.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Poop extends React.Component {
+    render(){
+    return (
+      <div>
+    <h1>Holy Moly</h1>
+    <h2>Pooooooooooooop</h2>
+    </div>
+    );
+  }
+}
+
+export default Poop;
